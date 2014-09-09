39a7f2bc36d19e925e6c0539e7834b621a41f9d2
Lione <lalushula@gmail.com>
2014-09-09 18:37:06 +0300

    final

---
+++ api/about.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>About</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div id="header">
+		<div>
+			
+			<div id="navigation">
+				<div>
+					<ul>
+					<li class="current"><a href="index.php">Home</a></li>
+						<li><a href="about.php">About us</a></li>
+						<li><a href="testPicture.php">User-Photos</a></li>
+						<li><a href="gallery.php">Gallery</a></li>
+						
+					</ul>
+					
+	
+				</div>
+				
+			</div>
+			
+			
+		</div>
+		</div>
+		
+		<div>
+		<div id="home">
+		<div id="contact">
+		<br/>
+		<br/><br/><br/><br/><br/><br/><br/><center>
+		<h3>An attempt at TwitPic photo scrapping by Lione Alushula</h3></center>
+		</div>
+		</div>
+		</div>
+
+</body>
+</html>
